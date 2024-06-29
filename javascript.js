14a4b9ffc52cb78e819b0f0127a0dd843a17eb48
Samual Lewis <lewissamuala@gmail.com>
2024-06-29 11:19:29 -0700

    Add styling and situational coloring

--- javascript.js
+++ javascript.js
@@ -13,20 +13,21 @@ TRACKED_ELEMENTS.set("winner", document.querySelector("#winner"));
 TRACKED_ELEMENTS.set("countdown", document.querySelector("#countdown"));
 TRACKED_ELEMENTS.set("score", document.querySelector("#score"));
 
-const WIN_MESSAGES = {};
-WIN_MESSAGES["rock"] = "You won! Rock smashes scissors!";
-WIN_MESSAGES["paper"] = "You won! Paper covers rock!";
-WIN_MESSAGES["scissors"] = "You won! Scissors cuts paper!";
-WIN_MESSAGES["final"] = "You beat the Computer! Congratulations!"
+const WIN_MESSAGES = new Map();
+WIN_MESSAGES.set("rock", "You won! Rock smashes scissors!");
+WIN_MESSAGES.set("paper", "You won! Paper covers rock!");
+WIN_MESSAGES.set("scissors", "You won! Scissors cuts paper!");
+WIN_MESSAGES.set("final", "You beat the Computer! Congratulations!");
 
-const LOSE_MESSAGES = {};
-LOSE_MESSAGES["rock"] = "You lose! Paper covers rock!";
-LOSE_MESSAGES["paper"] = "You lose! Scissors cuts paper!";
-LOSE_MESSAGES["scissors"] = "You lose! Rock smashes scissors!";
-LOSE_MESSAGES["final"] = "You lost to the Computer! Better luck next time!";
+const LOSE_MESSAGES = new Map();
+LOSE_MESSAGES.set("rock", "You lose! Paper covers rock!");
+LOSE_MESSAGES.set("paper", "You lose! Scissors cuts paper!");
+LOSE_MESSAGES.set("scissors", "You lose! Rock smashes scissors!");
+LOSE_MESSAGES.set("final", "You lost to the Computer. Better luck next time!");
 
-const TIE_MESSAGE = "You tied!";
-const TIE_MESSAGE_FINAL = "You tied with the Computer! So close!"
+const TIE_MESSAGES = new Map();
+TIE_MESSAGES.set("tie", "You tied!");
+TIE_MESSAGES.set("final", "You tied with the Computer. So close!");
 
 const DEFAULT_TEXT = new Map();
 DEFAULT_TEXT.set("humanChoice", TRACKED_ELEMENTS.get("humanChoice").innerText);
@@ -39,10 +40,13 @@ DEFAULT_TEXT.set("score", TRACKED_ELEMENTS.get("score").innerText);
 //create elements to be inserted later
 let finalWinner = document.createElement("p");
 finalWinner.setAttribute("id", "finalWinner");
+finalWinner.style.fontSize = "48px";
 
 const resetButton = document.createElement("button");
 resetButton.setAttribute("id", "reset");
 resetButton.innerText = "Reset";
+resetButton.style.fontSize = "32px";
+resetButton.style.padding = "16px 32px";
 resetButton.addEventListener("click", () => resetGame());
 
 
@@ -78,14 +82,14 @@ function playRound(humanChoice) {
       case "Rock":
         switch (computerChoice) {
           case "Rock":
-            TRACKED_ELEMENTS.get("winner").innerText = TIE_MESSAGE;
+            setTieText("tie");
             break;
           case "Paper":
-            TRACKED_ELEMENTS.get("winner").innerText = LOSE_MESSAGES["rock"];
+            setLoseText("rock");
             computerScore++;
             break;
           case "Scissors":
-            TRACKED_ELEMENTS.get("winner").innerText = WIN_MESSAGES["rock"];
+            setWinText("rock");
             humanScore++;
             break;
         }
@@ -93,14 +97,14 @@ function playRound(humanChoice) {
       case "Paper":
         switch (computerChoice) {
           case "Rock":
-            TRACKED_ELEMENTS.get("winner").innerText = WIN_MESSAGES["paper"];
+            setWinText("paper");
             humanScore++;
             break;
           case "Paper":
-            TRACKED_ELEMENTS.get("winner").innerText = TIE_MESSAGE;
+            setTieText("tie");
             break;
           case "Scissors":
-            TRACKED_ELEMENTS.get("winner").innerText = LOSE_MESSAGES["paper"];
+            setLoseText("paper");
             computerScore++;
             break;
         }
@@ -108,15 +112,15 @@ function playRound(humanChoice) {
       case "Scissors":
         switch (computerChoice) {
           case "Rock":
-            TRACKED_ELEMENTS.get("winner").innerText = LOSE_MESSAGES["scissors"];
+            setLoseText("scissors");
             computerScore++;
             break;
           case "Paper":
-            TRACKED_ELEMENTS.get("winner").innerText = WIN_MESSAGES["scissors"];
+            setWinText("scissors");
             humanScore++;
             break;
           case "Scissors":
-            TRACKED_ELEMENTS.get("winner").innerText = TIE_MESSAGE;
+            setTieText("tie");
             break;
         }
         break;
@@ -124,11 +128,14 @@ function playRound(humanChoice) {
     TRACKED_ELEMENTS.get("score").innerText = "You: " + humanScore + ", Computer: " + computerScore;
     if(currentRound === 6) {
       if(humanScore > computerScore) {
-        finalWinner.innerText = WIN_MESSAGES["final"];
+        finalWinner.innerText = WIN_MESSAGES.get("final");
+        finalWinner.style.color = "green";
       } else if (humanScore < computerScore) {
-        finalWinner.innerText = LOSE_MESSAGES["final"];
+        finalWinner.innerText = LOSE_MESSAGES.get("final");
+        finalWinner.style.color = "red";
       } else {
-        finalWinner.innerText = TIE_MESSAGE_FINAL;
+        finalWinner.innerText = TIE_MESSAGES.get("final");
+        finalWinner.style.color = "yellow";
       }
       output.appendChild(finalWinner);
       output.appendChild(resetButton);
@@ -141,8 +148,26 @@ function resetGame() {
   humanScore = 0;
   computerScore = 0;
   for(key of TRACKED_ELEMENTS.keys()) {
-    TRACKED_ELEMENTS.get(key).innerText = DEFAULT_TEXT.get(key);
+    const elem = TRACKED_ELEMENTS.get(key);
+    elem.innerText = DEFAULT_TEXT.get(key);
+    elem.style.color = "black";
   }
   output.removeChild(finalWinner);
   output.removeChild(resetButton);
+}
+
+function setWinText(choice) {
+  const elem = TRACKED_ELEMENTS.get("winner");
+  elem.innerText = WIN_MESSAGES.get(choice);
+  elem.style.color = "green";
+}
+function setLoseText(choice) {
+  const elem = TRACKED_ELEMENTS.get("winner");
+  elem.innerText = LOSE_MESSAGES.get(choice);
+  elem.style.color = "red";
+}
+function setTieText(choice) {
+  const elem = TRACKED_ELEMENTS.get("winner");
+  elem.innerText = TIE_MESSAGES.get(choice);
+  elem.style.color = "yellow";
 }
